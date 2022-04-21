aa70afd894b384ffc035b47b193abe9f605b9017
nahuoo <nahuel.sigismondi@gmail.com>
2022-04-21 11:55:05 -0300

    add Auth Controller & Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 
-
 @Module({
   imports: [AuthModule, BookmarkModule],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
